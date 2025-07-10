5058abbd466153aba1cbe335b260cd3bae78d09e
DecoMacie <119458392+DecoMacie@users.noreply.github.com>
2025-07-10 12:08:03 +0100

    Navbar Links
    Create Navbar links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,6 +4,14 @@ export default function NavBar() {
   return (
     <div>
       <Link to="/">Home</Link>
+      <Link to="/portfolio">Portfolio</Link>
+      <Link to="/services">Services</Link>
+      <Link to="/education">Education</Link>
+      <Link to="/media">Media</Link>
+      <Link to="/contacts">Connect</Link>
+      <a href="https://www.example.com">LinkdIn</a>
+      <a href="https://www.example.com">Instagram</a>
+      <a href="https://www.example.com">Github</a>
     </div>
   );
 }
