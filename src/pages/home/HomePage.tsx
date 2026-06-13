f54742e736e66969594d270910feb675099be30c
DecoMacie <119458392+DecoMacie@users.noreply.github.com>
2026-06-13 15:21:18 +0100

    Update HomePage.tsx
    Added ttitlefor featured repos section

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -12,19 +12,19 @@ export default function HomePage() {
       {/* HERO SECTION */}
       <div
         className="
-    mt-10
-    flex flex-col md:flex-row
-    items-center
-    justify-center
-    gap-8
-    p-6
-    bg-white
-    border border-[#E7E5E0]
-    rounded-lg
-    shadow-sm
-    hover:shadow-md
-    transition-shadow duration-300
-  "
+          mt-10
+          flex flex-col md:flex-row
+          items-center
+          justify-center
+          gap-8
+          p-6
+          bg-white
+          border border-[#E7E5E0]
+          rounded-lg
+          shadow-sm
+          hover:shadow-md
+          transition-shadow duration-300
+        "
       >
         {/* TEXT */}
         <div className="w-full md:w-1/2 text-center md:text-left">
@@ -34,22 +34,35 @@ export default function HomePage() {
         {/* IMAGE */}
         <img
           src={profileImage}
-          alt="profile image"
+          alt="Dercio Macie"
           className="
-      w-40 sm:w-52 md:w-72
-      h-auto
-      rounded-lg
-      object-cover
-      border-b-2 border-[#E7E5E0]
-    "
+            w-40 sm:w-52 md:w-72
+            h-auto
+            rounded-lg
+            object-cover
+            border border-[#E7E5E0]
+          "
         />
       </div>
+
+      <div className="mt-12 mb-6 border-t border-[#E7E5E0] pt-8"></div>
+
+      {/* FEATURED PROJECTS HEADER */}
+      <section className="mt-12 mb-6 text-center md:text-left">
+        <h2 className="text-3xl space-mono-bold text-[#2C2C2C] mb-2">
+          Featured Projects
+        </h2>
+      </section>
 
       {/* PROJECT GRID */}
       <div
         className="
-      mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6
-    "
+          grid grid-cols-1
+          sm:grid-cols-2
+          lg:grid-cols-3
+          xl:grid-cols-4
+          gap-6
+        "
       >
         {featuredRepos.map((repo) => (
           <Cards key={repo.name} cardData={repo} />
